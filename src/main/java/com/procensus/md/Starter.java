57a5373d366fc826a6820327a337f57c39125fbb
mdagcilar <md3g11@soton.ac.uk>
2018-05-15 21:18:01 +0100

    error handling for command line args

--- src/main/java/com/procensus/md/Starter.java
+++ src/main/java/com/procensus/md/Starter.java
@@ -2,25 +2,39 @@ package com.procensus.md;
 
 import org.apache.log4j.PropertyConfigurator;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Starter {
 
     public static void main(String[] args) {
         initialiseLogger();
+        GameManager gameManager = new GameManager();
 
-        if(args.length != 2){
-            System.out.println("To simulate Gnome Fortress please enter two command line arguments e.g.: 2 4");
-            System.out.println("Or to enter them now");
-        }else {
-            GameManager gameManager = new GameManager();
-            gameManager.start(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        // Error handling for command line arguments
+        if (args.length != 2) {
+            System.out.println("Incorrect command line arguments found");
+
+            Scanner scanner = new Scanner(System.in);
+            try {
+                System.out.println("Enter the number of Gnome teams you wish to simulate:");
+                int groups = scanner.nextInt();
+                System.out.println("Enter the number of Gnomes per team:");
+                int gnomesPerGroup = scanner.nextInt();
+                gameManager.start(groups, gnomesPerGroup);
+            } catch (InputMismatchException e) {
+                System.out.println("Incorrect argument entered please enter digits only");
+            }
+        } else {
+            try {
+                gameManager.start(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Program arguments must be integers");
+            }
         }
     }
 
     private static void initialiseLogger() {
         PropertyConfigurator.configure("resources/log4j.properties");
-    }
-
-    private void promptUserInput(){
-
     }
 }
